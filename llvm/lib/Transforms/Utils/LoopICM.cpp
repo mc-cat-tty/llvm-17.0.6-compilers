da4814680957b3e98a519d8b76cc57931fffc7be
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-05-14 10:32:26 +0200

    Implement dominance and deadness analysis

--- llvm/lib/Transforms/Utils/LoopICM.cpp
+++ llvm/lib/Transforms/Utils/LoopICM.cpp
@@ -6,6 +6,8 @@
 #include "llvm/ADT/SetOperations.h"
 #include "llvm/Support/GenericLoopInfo.h"
 #include <unordered_set>
+#include <vector>
+#include <functional>
 
 using namespace llvm;
 
@@ -32,15 +34,16 @@ void infoLog(Loop &l) {
   }
 }
 
-void licmOptimize(Loop &l) {
+void licmOptimize(Loop &l, LoopStandardAnalysisResults &lar) {
   using namespace std;
 
-  auto loopInvariantInstructions = unordered_set<Instruction*>{};
+  using Granularity = Instruction*;
+  auto loopInvariantInstructions = unordered_set<Granularity>{};
 
   // Be aware that this lambda marks phi-instructions as loop-invariant
   auto isOperandLI = [&l, &loopInvariantInstructions] (const Use &usee) {
-    if (dyn_cast<Constant>(usee)) return true;
-    if (dyn_cast<Argument>(usee)) return true;
+    if (isa<Constant>(usee)) return true;
+    if (isa<Argument>(usee)) return true;
     
     decltype(auto) i = dyn_cast<Instruction>(usee);
     
@@ -49,18 +52,25 @@ void licmOptimize(Loop &l) {
 
     return false;
   };
+
+  auto isInstructionLI = [&isOperandLI](const Instruction &i) {
+    if (isa<PHINode>(i)) return false;
+
+    bool isInstrLI = true;
+    // If the loop-invariance condition holds for each operand
+    for (const auto &usee : i.operands()) isInstrLI &= isOperandLI(usee);
+    return isInstrLI;
+  };
   
   // Extract loop-invariant instructions with iterative algo
   for (auto *bb : l.blocks()) {
     for (auto &i : *bb) {
-      bool isInstrLI = true;
-      for (const auto &usee : i.operands()) isInstrLI &= isOperandLI(usee);
-
-      if (isInstrLI) loopInvariantInstructions.insert(&i);
+      if (isInstructionLI(i)) loopInvariantInstructions.insert(&i);
     }
   }
 
-
+  // Printing loop-invariant instructions
+  errs() << "LOOP-INVARIANT INSTRUCTIONS\n";
   for (const auto *i : loopInvariantInstructions) {
     i->print(errs());
     errs() << "\n";
@@ -70,19 +80,51 @@ void licmOptimize(Loop &l) {
   using Edge = pair<BasicBlock*, BasicBlock*>;
   auto exitEdges = SmallVector<Edge>{};
   l.getExitEdges(exitEdges);
-  const auto parentFunction = l.getBlocks()[0]->getParent();
-  const auto dominators = DominatorTree(*parentFunction);;
-  auto doesDominateExitPoint = [&exitEdges, &dominators] (Instruction *instr) {
-    bool dominate = true;
-    for (const auto edge : exitEdges) dominate &= dominators.dominates(instr, edge.second);
-    return dominate;
+  const auto &dominators = lar.DT;
+
+  auto isDeadOutsideLoop = [&l] (const Instruction *instr) {
+    for (auto u : instr->users()) {
+      decltype(auto) i = dyn_cast<Instruction>(u);
+      if (not l.contains(i)) return false;
+    }
+
+    return true;
   };
+
+  auto isMovable = [&exitEdges, &dominators, &isDeadOutsideLoop] (const Instruction *instr) {
+    for (const auto &edge : exitEdges)
+      if (
+        not dominators.dominates(instr, edge.second)
+        and not isDeadOutsideLoop(instr)
+      ) return false;
+    
+    return true;
+  };
+
+  // Discarding non-movable instructions
+  auto movableInstructions =
+    vector<Granularity>(
+      begin(loopInvariantInstructions),
+      end(loopInvariantInstructions)
+    );
+  movableInstructions.erase(
+    remove_if(begin(movableInstructions), end(movableInstructions), not_fn(isMovable)),
+    end(movableInstructions)
+  );
+  unordered_set<Granularity>().swap(loopInvariantInstructions);  // Make loopInvariantInstructions go out of scope
+
+  // Printing movable instructions
+  errs() << "MOVABLE INSTRUCTIONS\n";
+  for (const auto *i : movableInstructions) {
+    i->print(errs());
+    errs() << "\n";
+  }
 }
 
 PreservedAnalyses LoopICM::run(Loop &l, LoopAnalysisManager &lam, LoopStandardAnalysisResults &lar, LPMUpdater &lu) {
   // infoLog(l);
 
-  licmOptimize(l);
+  licmOptimize(l, lar);
 
   return PreservedAnalyses::all();
 }
