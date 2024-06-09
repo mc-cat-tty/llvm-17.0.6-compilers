e6a275f84cfd4ba37db71cb3fe65da70e8db449d
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-06-09 11:47:28 +0200

    WIP: loop fusion for rotated loops

--- llvm/lib/Transforms/Utils/LoopFusion.cpp
+++ llvm/lib/Transforms/Utils/LoopFusion.cpp
@@ -4,38 +4,48 @@
 #include "llvm/Analysis/PostDominators.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/DependenceAnalysis.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
+BasicBlock* getPreheaderOrGuard(Loop *l) {
+    return l->isGuarded() ?
+        l->getLoopGuardBranch()->getParent()
+        : l->getLoopPreheader();
+}
 
 bool isAdjacent(Loop *prevLoop, Loop *nextLoop){
-
     // if the loop has multiple exits the function returns null 
-    auto prevExitBB = prevLoop->getExitBlock(); 
+    auto prevExitBB = prevLoop->isGuarded() ? 
+        prevLoop->getExitBlock()->getSingleSuccessor()  // Get block after critical edge
+        : prevLoop->getExitBlock();
 
     // loop with multiple exits can't be fused
     if (not prevExitBB) return false; 
 
     // if the loop is guarded the entry block to check 
     // is the block which contains the guard branch
-    auto nextPreheader = nextLoop->isGuarded() ? 
-        nextLoop->getLoopGuardBranch()->getParent() : nextLoop->getLoopPreheader();
+    auto nextEntryBB = getPreheaderOrGuard(nextLoop);
+    
+    // prevExitBB->print(errs());
+    // nextPreheader->print(errs());
 
-    prevExitBB->print(errs());
-    nextPreheader->print(errs());
-    errs() << "Result: " << (prevExitBB == nextPreheader);
-    return prevExitBB == nextPreheader;
+    auto ret = (prevExitBB == nextEntryBB);
+    errs() << "Are adjacent? " << ret << "\n";
+    return ret;
 }
 
 bool isControlFlowEquivalent(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, Loop *nextLoop){
     DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
     PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
     
-    auto prevPreheader = prevLoop->getLoopPreheader();
-    auto nextPreheader = nextLoop->getLoopPreheader();
+    auto prevPreheader = getPreheaderOrGuard(prevLoop);
+    auto nextPreheader = getPreheaderOrGuard(nextLoop);
 
     // return true iff the prevLoop dominates the nextLoop
     // and iff the nextLoop postdominates the prevLoop
-    return DT.dominates(prevPreheader, nextPreheader) and PDT.dominates(nextPreheader, prevPreheader);
+    auto ret = DT.dominates(prevPreheader, nextPreheader) and PDT.dominates(nextPreheader, prevPreheader);
+    errs() << "Are CF equivalent? " << ret << "\n";
+    return ret;
 }
 
 bool hasSameTripCount(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, Loop *nextLoop){
@@ -54,23 +64,24 @@ bool hasSameTripCount(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, 
     if(isa<SCEVCouldNotCompute>(prevBackedgeCount) or isa<SCEVCouldNotCompute>(nextBackedgeCount)) return false;
     
     // returns true if SCEV objects are equal, false otherwise
-    return SE.isKnownPredicate(ICmpInst::ICMP_EQ, prevBackedgeCount, nextBackedgeCount);
+    auto ret = SE.isKnownPredicate(ICmpInst::ICMP_EQ, prevBackedgeCount, nextBackedgeCount);
+    errs() << "Have same TC? " << ret << "\n";
+    return ret;
 }
 
 /**
- * Checks for any R-W or W-R dependency between the pair of loops passed to the function.
+ * Checks for any R-W, W-W, or W-R dependency between the pair of loops passed to the function.
  * As in the original LLVM's loop-fusion pass, there is no real distinction between negative/positive/neutral dependencies.
  * Any form of dependcy is prohibited in order to activate the optimization. 
 */
-bool hasNegativeDistanceDependencies(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, Loop *nextLoop){
+bool hasNotNegativeDistanceDependencies(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, Loop *nextLoop){
     errs() << "Checking negative dependencies\n";
     
     DependenceInfo &DI = AM.getResult<DependenceAnalysis>(F);
     SmallVector<Instruction*> prevStores, prevLoads, nextLoads, nextStores;
 
     auto areDependent = [&DI] (Instruction *x, Instruction *y) {
-        auto dep = DI.depends(x, y, true);
-        return dep and not dep->isConfused();
+        return DI.depends(x, y, true);
     };
     
     for (auto *bb : prevLoop->blocks()) {
@@ -83,16 +94,19 @@ bool hasNegativeDistanceDependencies(Function &F, FunctionAnalysisManager &AM, L
     for (auto *bb : nextLoop->blocks()) {
         for (auto &i : *bb) {
             if (i.mayWriteToMemory()) nextStores.push_back(&i);
-            if (i.mayReadFromMemory()) nextLoads.push_back(&i);        }
+            if (i.mayReadFromMemory()) nextLoads.push_back(&i);
+        }
     }
 
     // Store-load, Load-store access patterns can cause problems
     for (auto &prevI : prevStores) {
+        for (auto &nextI : nextStores) if (areDependent(prevI, nextI)) return false;
         for (auto &nextI : nextLoads) if (areDependent(prevI, nextI)) return false;
     }
 
-    for (auto &prevI : prevLoads) {
-        for (auto &nextI : nextStores) if (areDependent(prevI, nextI)) return false;
+    for (auto &nextI : nextStores) {
+        for (auto &prevI : prevStores) if (areDependent(prevI, nextI)) return false;
+        for (auto &prevI : prevLoads) if (areDependent(prevI, nextI)) return false;
     }
 
     return true;
@@ -100,21 +114,57 @@ bool hasNegativeDistanceDependencies(Function &F, FunctionAnalysisManager &AM, L
 
 Loop * optimize(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, Loop *nextLoop){
     ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
-  
-    errs() << "Optimizable\n"; 
-    
+    errs() << "Starting the Loop Fusion\n";
+
+    // Get CFG blocks 
+    auto prevPreheder = prevLoop->getLoopPreheader();
+    auto prevLatch = prevLoop->getLoopLatch();
+    auto prevBody = prevLatch->getSinglePredecessor();
+    auto prevBodyEntry = prevPreheder->getSingleSuccessor();
+    auto prevExit = prevLoop->getExitBlock();
+    auto prevGuard = prevLoop->getLoopGuardBranch();
+
+    auto nextPreheader = nextLoop->getLoopPreheader();
+    auto nextLatch = nextLoop->getLoopLatch();
+    auto nextBody = nextLatch->getSinglePredecessor();
+    auto nextBodyEntry = nextPreheader->getSingleSuccessor();
+    auto nextExit = nextLoop->getExitBlock();
+
+    // Update next loop's induction var with the one from prev loop
+    auto prevIV = prevLoop->getInductionVariable(SE);
+    auto prevValueIV = dyn_cast<Value>(prevIV);
+
     auto nextIV = nextLoop->getInductionVariable(SE);
+    auto nextValueIV = dyn_cast<Value>(nextIV);
+    
+    nextValueIV->replaceAllUsesWith(prevValueIV);
+    nextIV->eraseFromParent();
 
-/*   for(auto userIter = nextIV->user_begin(); userIter != nextIV->user_end(); ++userIter){
-        User *user = *userIter;
-
-        user->print(errs());
+    // Move phi nodes from next loop's body to prev one
+    nextBodyEntry->replacePhiUsesWith(nextLatch, prevLatch);
+    nextBodyEntry->replacePhiUsesWith(nextPreheader, prevPreheder);
+    
+    SmallVector<Instruction*> toBeMoved;
+    for (Instruction &nextInst : *nextBodyEntry) {
+        nextInst.print(errs());
         errs() << "\n";
-    }*/
+        if (isa<PHINode>(nextInst)) toBeMoved.emplace_back(&nextInst);
+    }
 
+    const auto &movePoint = prevBodyEntry->getFirstNonPHI();
+    for (auto *i : toBeMoved) i->moveBefore(movePoint);
 
-    // when optmized the function must return the new fused loop 
-    // atm with no optimization returns the current loop 
+    // Edit CFG to reflect fusion changes
+    prevLatch->getTerminator()->setSuccessor(1, nextExit);
+    prevBody->getTerminator()->replaceSuccessorWith(prevLatch, nextBodyEntry);
+    nextPreheader->replacePhiUsesWith(prevLatch, prevBody);
+    nextBody->getTerminator()->replaceSuccessorWith(nextLatch, prevLatch);
+    if(prevGuard) prevGuard->setSuccessor(1, nextExit);
+
+    // Clean up unreachable blocks
+    EliminateUnreachableBlocks(F);
+
+    // Return the first loop of the fused block
     return prevLoop;
 }
 
@@ -122,10 +172,11 @@ Loop * optimize(Function &F, FunctionAnalysisManager &AM, Loop *prevLoop, Loop *
 PreservedAnalyses LoopFusion::run(Function &F, FunctionAnalysisManager &AM) {
     LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
     
-    errs() << "Started Loop Fusion\n";
+    errs() << "Checking conditions\n";
 
     Loop *prevLoop = nullptr;
     bool optimizable = false;
+    bool modified = false;
     int num = 0;
 
     for (auto lit = LI.rbegin(); lit != LI.rend(); ++lit){
@@ -135,13 +186,12 @@ PreservedAnalyses LoopFusion::run(Function &F, FunctionAnalysisManager &AM) {
             optimizable = isAdjacent(prevLoop, *lit)   
                 and isControlFlowEquivalent(F, AM, prevLoop, *lit) 
                 and hasSameTripCount(F, AM, prevLoop, *lit)
-                and not hasNegativeDistanceDependencies(F, AM, prevLoop, *lit);
-        
-        errs() << optimizable << "\n";
-        
+                and hasNotNegativeDistanceDependencies(F, AM, prevLoop, *lit);
+            
+        if(optimizable) modified = true;
         prevLoop = optimizable ? optimize(F, AM, prevLoop, *lit) : *lit;
 
     }
 
-    return PreservedAnalyses::all();
+    return modified ? PreservedAnalyses::none() : PreservedAnalyses::all();
 }
